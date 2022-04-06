103b55c0c91912d1cbc80bebdfb71486ef8f88f4
webreznov-studio <webreznov.landing@gmail.com>
2022-04-06 17:37:41 +0300

    update template

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+# Use components library: grommet
+
+
 <!-- AUTO-GENERATED-CONTENT:START (STARTER) -->
 <p align="center">
   <a href="https://www.gatsbyjs.com">
--- src/components/Header/components/LogoMenu/LogoMenu.jsx
+++ src/components/Header/components/LogoMenu/LogoMenu.jsx
@@ -1,17 +1,13 @@
 import React, {useState} from "react";
 import {Link} from "gatsby";
 import logo from "../../../../images/logo.png";
-import {MenuList} from "../MenuList/MenuList";
 
 export const LogoMenu = () => {
-    const [isShow, setIsShow] = useState(false);
-
     return (
-        <div onClick={() => setIsShow(!isShow)}>
+        <div>
             <Link to="#">
                 <img src={logo} alt="" />
             </Link>
-            <MenuList isShow={isShow} onClose={setIsShow} />
         </div>
     )
 }
